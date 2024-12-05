42ae088544259e16b7bdc101393622af163db1e1
6ix78 <87336515+6ix78@users.noreply.github.com>
2024-12-05 20:39:08 +0100

    Update HeadingLink.tsx

--- src/app/components/HeadingLink.tsx
+++ src/app/components/HeadingLink.tsx
@@ -85,3 +85,12 @@ export const HeadingLink: React.FC<HeadingLinkProps> = ({
         </Flex>
     );
 };
+
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '75803b411ce666b17dce79114ad539a76be47d86';
+window.smartsupp||(function(d) {
+	var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+	s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+	c.type='text/javascript';c.charset='utf-8';c.async=true;
+	c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
